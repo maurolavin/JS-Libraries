d118694785f7a49f5b176efbb677a717c54734e8
mauro.lavin <villarroelmauroemanuel@gmail.com>
2022-08-04 23:13:38 -0300

    JS Libraries. SweetAlert y Toastify

---
+++ index.js
@@ -0,0 +1,122 @@
+Swal.fire({
+    title: 'Atencion!',
+    text: 'Este conversor de unidades esta en desarrollo. Puede tener errores y problemas para funcionar.',
+    confirmButtonText: 'Entiendo :)',
+    confirmButtonColor: '#89b136',
+
+  })
+
+var valor = document.getElementById("valor");
+valor.addEventListener('keyup', conversion);
+conversionResult = document.getElementById("result");
+
+unit1 = document.getElementById("unit1");
+unit1.addEventListener('change', conversion);
+
+unit2= document.getElementById("unit2");
+unit2.addEventListener('change', conversion);
+
+var result;
+
+function conversion(){
+    if(valor.value==''){
+        return;
+    }
+
+    number = valor.value;
+    number = parseFloat(number);
+
+    if(unit1.value=="kilometer" && unit2.value=="kilometer"){
+        result = number * 1;
+        conversionResult.innerHTML = number + ' ' + unit1.value + " equivalen a " + result + " " + unit2.value;
+    }
+
+    if(unit1.value=="kilometer" && unit2.value=="meter"){
+        result = number * 1000;
+        conversionResult.innerHTML = number + ' ' + unit1.value + " equivalen a " + result + " " + unit2.value;
+    }
+
+    if(unit1.value=="kilometer" && unit2.value=="centimeter"){
+        result = number * 10000;
+        conversionResult.innerHTML = number + ' ' + unit1.value + " equivalen a " + result + " " + unit2.value;
+    }
+
+    if(unit1.value=="centimeter" && unit2.value=="centimeter"){
+        result = number * 1;
+        conversionResult.innerHTML = number + ' ' + unit1.value + " equivalen a " + result + " " + unit2.value;
+    }
+
+    if(unit1.value=="centimeter" && unit2.value=="meter"){
+        result = number / 100;
+        conversionResult.innerHTML = number + ' ' + unit1.value + " equivalen a " + result + " " + unit2.value;
+    }
+
+    if(unit1.value=="centimeter" && unit2.value=="kilometer"){
+        result = number / 1000;
+        conversionResult.innerHTML = number + ' ' + unit1.value + " equivalen a " + result + " " + unit2.value;
+    }
+
+    if(unit1.value=="meter" && unit2.value=="meter"){
+        result = number * 1;
+        conversionResult.innerHTML = number + ' ' + unit1.value + " equivalen a " + result + " " + unit2.value;
+    }
+    
+    if(unit1.value=="meter" && unit2.value=="centimeter"){
+        result = number * 100;
+        conversionResult.innerHTML = number + ' ' + unit1.value + " equivalen a " + result + " " + unit2.value;
+    }
+
+    if(unit1.value=="meter" && unit2.value=="kilometer"){
+        result = number / 1000;
+        conversionResult.innerHTML = number + ' ' + unit1.value + " equivalen a " + result + " " + unit2.value;
+    }
+
+    if(unit1.value=="miles" && unit2.value=="miles"){
+        result = number * 1;
+        conversionResult.innerHTML = number + ' ' + unit1.value + " equivalen a " + result + " " + unit2.value;
+    }
+
+    if(unit1.value=="miles" && unit2.value=="kilometer"){
+        result = number * 1.60934;
+        conversionResult.innerHTML = number + ' ' + unit1.value + " equivalen a " + result + " " + unit2.value;
+    }
+
+    if(unit1.value=="miles" && unit2.value=="centimeter"){
+        result = number / 0.0000062137;
+        conversionResult.innerHTML = number + ' ' + unit1.value + " equivalen a " + result + " " + unit2.value;
+    }
+
+    if(unit1.value=="miles" && unit2.value=="meter"){
+        result = number / 0.00062137;
+        conversionResult.innerHTML = number + ' ' + unit1.value + " equivalen a " + result + " " + unit2.value;
+    }
+
+    if(unit1.value=="meter" && unit2.value=="miles"){
+        result = number * 0.00062137;
+        conversionResult.innerHTML = number + ' ' + unit1.value + " equivalen a " + result + " " + unit2.value;
+    }
+    
+    if(unit1.value=="centimeter" && unit2.value=="miles"){
+        result = number * 0.0000062137;
+        conversionResult.innerHTML = number + ' ' + unit1.value + " equivalen a " + result + " " + unit2.value;
+    }
+    
+    if(unit1.value=="kilometer" && unit2.value=="miles"){
+        result = number / 0.62137;
+        conversionResult.innerHTML = number + ' ' + unit1.value + " equivalen a " + result + " " + unit2.value;
+    }
+
+    Toastify({
+        text: "Conversion exitosa!",
+        duration: 1500,
+        className: "success",
+        destination: "https://github.com/maurolavin",
+        newWindow: true,
+        gravity: "top",
+        position: "center",
+        onClick: function(){} // Callback after click
+    }).showToast();
+}
+
+
+
